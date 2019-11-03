494b0681f831c76b28e0bcd02cd1b83378416e01
Héctor Ramón Jiménez <hector0193@gmail.com>
2019-11-03 05:06:53 +0100

    Enable debug view explicitly and test it in CI

--- winit/src/lib.rs
+++ winit/src/lib.rs
@@ -9,11 +9,8 @@ pub use application::Application;
 
 // We disable debug capabilities on release builds unless the `debug` feature
 // is explicitly enabled.
-#[cfg_attr(any(debug_assertions, feature = "debug"), path = "debug/basic.rs")]
-#[cfg_attr(
-    not(any(debug_assertions, feature = "debug")),
-    path = "debug/null.rs"
-)]
+#[cfg_attr(feature = "debug", path = "debug/basic.rs")]
+#[cfg_attr(not(feature = "debug"), path = "debug/null.rs")]
 mod debug;
 
 use debug::Debug;
