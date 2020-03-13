0b12d706e3767c04b89d4d2692c31ff9fa715cc9
Héctor Ramón Jiménez <hector0193@gmail.com>
2020-03-13 07:54:14 +0100

    Unfocus pane in `PaneGrid` on out-of-bounds click

--- native/src/widget/pane_grid.rs
+++ native/src/widget/pane_grid.rs
@@ -179,6 +179,8 @@ where
                                 };
                             }
                         }
+                    } else {
+                        self.state.focused_pane = FocusedPane::None;
                     }
                 }
                 ButtonState::Released => {
