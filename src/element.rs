1d5decb2fdc691d8fc07158d59152ca286a76f8b
Sebastian Imlay <sebastian.imlay@gmail.com>
2020-04-16 00:47:29 -0700

    Got it to commit for ios runtiem

--- src/element.rs
+++ src/element.rs
@@ -1,9 +1,13 @@
 /// A generic widget.
 ///
 /// This is an alias of an `iced_native` element with a default `Renderer`.
-#[cfg(not(target_arch = "wasm32"))]
+#[cfg(all(not(target_arch = "wasm32"), not(target_os = "ios")))]
 pub type Element<'a, Message> =
     crate::runtime::Element<'a, Message, crate::renderer::Renderer>;
 
+#[cfg(target_os = "ios")]
+pub use iced_ios::Element;
+
 #[cfg(target_arch = "wasm32")]
 pub use iced_web::Element;
+
