b639b24c9a2a8bb7c2f66262a7854f9a7aca10d9
Héctor Ramón Jiménez <hector0193@gmail.com>
2020-06-08 10:04:13 +0200

    Fix consistency of `ProgressBar` docs

--- glow/src/widget/progress_bar.rs
+++ glow/src/widget/progress_bar.rs
@@ -1,4 +1,9 @@
-//! Display progress to your uses with a bar
+//! Allow your users to visually track the progress of a computation.
+//!
+//! A [`ProgressBar`] has a range of possible values and a current value,
+//! as well as a length, height and style.
+//!
+//! [`ProgressBar`]: type.ProgressBar.html
 use crate::Renderer;
 
 pub use iced_graphics::progress_bar::{Style, StyleSheet};
