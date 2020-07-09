3c5921f30c17bcbe0af74a30b6a256a3fa03515c
Héctor Ramón Jiménez <hector0193@gmail.com>
2020-07-09 07:05:57 +0200

    Update `pane_grid` GIFs

--- native/src/widget/pane_grid.rs
+++ native/src/widget/pane_grid.rs
@@ -36,7 +36,7 @@ use crate::{
 /// A collection of panes distributed using either vertical or horizontal splits
 /// to completely fill the space available.
 ///
-/// [![Pane grid - Iced](https://thumbs.gfycat.com/MixedFlatJellyfish-small.gif)](https://gfycat.com/mixedflatjellyfish)
+/// [![Pane grid - Iced](https://thumbs.gfycat.com/FrailFreshAiredaleterrier-small.gif)](https://gfycat.com/frailfreshairedaleterrier)
 ///
 /// This distribution of space is common in tiling window managers (like
 /// [`awesome`](https://awesomewm.org/), [`i3`](https://i3wm.org/), or even
