bab71971fb6653c81e74a517c9ace93be63b49fc
Kaiden42 <gitlab@tinysn.com>
2020-09-19 20:20:14 +0200

    fix format

--- glow/src/widget/toggler.rs
+++ glow/src/widget/toggler.rs
@@ -4,6 +4,6 @@ use crate::Renderer;
 pub use iced_graphics::toggler::{Style, StyleSheet};
 
 /// A toggler that can be toggled.
-/// 
+///
 /// This is an alias of an `iced_native` checkbox with an `iced_wgpu::Renderer`.
 pub type Toggler<Message> = iced_native::Toggler<Message, Renderer>;
