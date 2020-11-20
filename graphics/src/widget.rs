3296be845c1f03bd481e10912ffdea74ac5ab1f6
Héctor Ramón Jiménez <hector0193@gmail.com>
2020-11-20 10:13:58 +0100

    Implement `QRCode` widget

--- graphics/src/widget.rs
+++ graphics/src/widget.rs
@@ -63,3 +63,11 @@ pub mod canvas;
 #[cfg(feature = "canvas")]
 #[doc(no_inline)]
 pub use canvas::Canvas;
+
+#[cfg(feature = "qr_code")]
+#[cfg_attr(docsrs, doc(cfg(feature = "qr_code")))]
+pub mod qr_code;
+
+#[cfg(feature = "qr_code")]
+#[doc(no_inline)]
+pub use qr_code::QRCode;
