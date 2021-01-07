e7344d03b467e87b3e50fa8d7ecd74994b46a4e6
Héctor Ramón Jiménez <hector0193@gmail.com>
2021-01-07 21:07:44 +0100

    Use `BTreeMap` for splits and regions in `pane_grid`
    This preserves ordering between calls to update and draw logic.

--- native/src/widget/pane_grid/pane.rs
+++ native/src/widget/pane_grid/pane.rs
@@ -1,5 +1,5 @@
 /// A rectangular region in a [`PaneGrid`] used to display widgets.
 ///
 /// [`PaneGrid`]: crate::widget::PaneGrid
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Pane(pub(super) usize);
