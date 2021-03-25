883c7e71ae699a29c23b8f95b7335d015e5a985d
Héctor Ramón Jiménez <hector0193@gmail.com>
2021-03-25 11:27:31 +0100

    Introduce `internal_backend` to `iced_wgpu::Settings`

--- wgpu/src/settings.rs
+++ wgpu/src/settings.rs
@@ -16,6 +16,9 @@ pub struct Settings {
     /// [`Backend`]: crate::Backend
     pub present_mode: wgpu::PresentMode,
 
+    /// The internal graphics backend to use.
+    pub internal_backend: wgpu::BackendBit,
+
     /// The bytes of the font that will be used by default.
     ///
     /// If `None` is provided, a default system font will be chosen.
@@ -30,14 +33,52 @@ pub struct Settings {
     pub antialiasing: Option<Antialiasing>,
 }
 
+impl Settings {
+    /// Creates new [`Settings`] using environment configuration.
+    ///
+    /// Specifically:
+    ///
+    /// - The `internal_backend` can be configured using the `WGPU_BACKEND`
+    /// environment variable. If the variable is not set, the primary backend
+    /// will be used. The following values are allowed:
+    ///     - `vulkan`
+    ///     - `metal`
+    ///     - `dx12`
+    ///     - `dx11`
+    ///     - `gl`
+    ///     - `webgpu`
+    pub fn from_env() -> Self {
+        Settings {
+            internal_backend: backend_from_env()
+                .unwrap_or(wgpu::BackendBit::PRIMARY),
+            ..Self::default()
+        }
+    }
+}
+
 impl Default for Settings {
     fn default() -> Settings {
         Settings {
             format: wgpu::TextureFormat::Bgra8UnormSrgb,
             present_mode: wgpu::PresentMode::Mailbox,
+            internal_backend: wgpu::BackendBit::PRIMARY,
             default_font: None,
             default_text_size: 20,
             antialiasing: None,
         }
     }
+}
+
+fn backend_from_env() -> Option<wgpu::BackendBit> {
+    std::env::var("WGPU_BACKEND").ok().map(|backend| {
+        match backend.to_lowercase().as_str() {
+            "vulkan" => wgpu::BackendBit::VULKAN,
+            "metal" => wgpu::BackendBit::METAL,
+            "dx12" => wgpu::BackendBit::DX12,
+            "dx11" => wgpu::BackendBit::DX11,
+            "gl" => wgpu::BackendBit::GL,
+            "webgpu" => wgpu::BackendBit::BROWSER_WEBGPU,
+            other => panic!("Unknown backend: {}", other),
+        }
+    })
 }
