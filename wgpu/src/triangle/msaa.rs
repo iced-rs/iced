4cbc34524598756ce18cb25a664bc6f256d42851
Héctor Ramón <hector@lich.io>
2021-05-21 20:34:08 +0700

    Use `FilterMode::Nearest` in `triangle::msaa`

--- wgpu/src/triangle/msaa.rs
+++ wgpu/src/triangle/msaa.rs
@@ -20,9 +20,9 @@ impl Blit {
             address_mode_u: wgpu::AddressMode::ClampToEdge,
             address_mode_v: wgpu::AddressMode::ClampToEdge,
             address_mode_w: wgpu::AddressMode::ClampToEdge,
-            mag_filter: wgpu::FilterMode::Linear,
-            min_filter: wgpu::FilterMode::Linear,
-            mipmap_filter: wgpu::FilterMode::Linear,
+            mag_filter: wgpu::FilterMode::Nearest,
+            min_filter: wgpu::FilterMode::Nearest,
+            mipmap_filter: wgpu::FilterMode::Nearest,
             ..Default::default()
         });
 
