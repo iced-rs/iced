dbfe6b207b218539c7d324c9b3819982b23f1046
Andrew Wheeler <genusistimelord@gmail.com>
2021-06-08 09:33:21 -0400

    Implemented Selection list Example

---
+++ examples/selection_list/README.md
@@ -0,0 +1,18 @@
+## Selection-list
+
+A list of selectable options.
+
+It displays and positions an overlay.
+
+The __[`main`]__ file contains all the code of the example.
+
+<div align="center">
+  <img src="https://user-images.githubusercontent.com/518289/87125075-2c232e80-c28a-11ea-95c2-769c610b8843.gif">
+</div>
+
+You can run it with `cargo run`:
+```
+cargo run --package selection_list
+```
+
+[`main`]: src/main.rs
---
+++ examples/selection_list/src/main.rs
@@ -0,0 +1,128 @@
+use iced::{
+    scrollable, selection_list, Align, Container, Element, Length, Sandbox,
+    Scrollable, SelectionList, Settings, Space, Text,
+};
+
+pub fn main() -> iced::Result {
+    Example::run(Settings::default())
+}
+
+#[derive(Default)]
+struct Example {
+    scroll: scrollable::State,
+    vec: Vec<String>,
+    selection_list: selection_list::State<String>,
+    selected_language: String,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    LanguageSelected(String),
+}
+
+impl Sandbox for Example {
+    type Message = Message;
+
+    fn new() -> Self {
+        let mut vec = Vec::with_capacity(10);
+
+        for i in Language::ALL.iter() {
+            vec.push(format!("{:?}", i))
+        }
+
+        Self {
+            vec,
+            ..Default::default()
+        }
+    }
+
+    fn title(&self) -> String {
+        String::from("Selection list - Iced")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::LanguageSelected(language) => {
+                self.selected_language = language.clone();
+
+                if language == "Rust" {
+                    self.vec.push("Rusty".into());
+                }
+            }
+        }
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        let selection_list = SelectionList::new(
+            &mut self.selection_list,
+            &self.vec[..],
+            Some(self.selected_language.clone()),
+            Message::LanguageSelected,
+        );
+
+        let mut content = Scrollable::new(&mut self.scroll)
+            .width(Length::Fill)
+            .align_items(Align::Center)
+            .spacing(10)
+            .push(Space::with_height(Length::Units(600)))
+            .push(selection_list)
+            .push(Text::new("Which is your favorite language?"))
+            .push(Text::new(format!("{:?}", self.selected_language)));
+
+        content = content.push(Space::with_height(Length::Units(600)));
+
+        Container::new(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_x()
+            .center_y()
+            .into()
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Language {
+    Rust,
+    Elm,
+    Ruby,
+    Haskell,
+    C,
+    Javascript,
+    Other,
+}
+
+impl Language {
+    const ALL: [Language; 7] = [
+        Language::C,
+        Language::Elm,
+        Language::Ruby,
+        Language::Haskell,
+        Language::Rust,
+        Language::Javascript,
+        Language::Other,
+    ];
+}
+
+impl Default for Language {
+    fn default() -> Language {
+        Language::Rust
+    }
+}
+
+impl std::fmt::Display for Language {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Language::Rust => "Rust",
+                Language::Elm => "Elm",
+                Language::Ruby => "Ruby",
+                Language::Haskell => "Haskell",
+                Language::C => "C",
+                Language::Javascript => "Javascript",
+                Language::Other => "Some other language",
+            }
+        )
+    }
+}
