2993e9b46674d976443abe1380c0e2b54f934a8e
Héctor Ramón Jiménez <hector0193@gmail.com>
2021-07-15 16:43:52 +0200

    Fix implementation of `Widget::overlay` for `pane_grid::TitleBar`

--- native/src/widget/pane_grid/title_bar.rs
+++ native/src/widget/pane_grid/title_bar.rs
@@ -248,6 +248,22 @@ where
         &mut self,
         layout: Layout<'_>,
     ) -> Option<overlay::Element<'_, Message, Renderer>> {
-        self.content.overlay(layout)
+        let mut children = layout.children();
+        let padded = children.next().unwrap();
+
+        let mut children = padded.children();
+        let title_layout = children.next().unwrap();
+
+        let Self {
+            content, controls, ..
+        } = self;
+
+        content.overlay(title_layout).or_else(move || {
+            controls.as_mut().and_then(|controls| {
+                let controls_layout = children.next().unwrap();
+
+                controls.overlay(controls_layout)
+            })
+        })
     }
 }
