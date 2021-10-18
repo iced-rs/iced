945f69e567d1766c82f77d3299d2d619d56d3add
Héctor Ramón Jiménez <hector0193@gmail.com>
2021-10-18 15:44:03 +0700

    Draw styling in `Widget::draw` for `Container`

--- native/src/widget/container.rs
+++ native/src/widget/container.rs
@@ -7,8 +7,8 @@ use crate::layout;
 use crate::overlay;
 use crate::renderer;
 use crate::{
-    Clipboard, Element, Hasher, Layout, Length, Padding, Point, Rectangle,
-    Widget,
+    Background, Clipboard, Color, Element, Hasher, Layout, Length, Padding,
+    Point, Rectangle, Widget,
 };
 
 use std::u32;
@@ -27,7 +27,7 @@ pub struct Container<'a, Message, Renderer> {
     max_height: u32,
     horizontal_alignment: alignment::Horizontal,
     vertical_alignment: alignment::Vertical,
-    style: &'a dyn StyleSheet,
+    style_sheet: &'a dyn StyleSheet,
     content: Element<'a, Message, Renderer>,
 }
 
@@ -48,7 +48,7 @@ where
             max_height: u32::MAX,
             horizontal_alignment: alignment::Horizontal::Left,
             vertical_alignment: alignment::Vertical::Top,
-            style: Default::default(),
+            style_sheet: Default::default(),
             content: content.into(),
         }
     }
@@ -108,8 +108,8 @@ where
     }
 
     /// Sets the style of the [`Container`].
-    pub fn style(mut self, style: &'a dyn StyleSheet) -> Self {
-        self.style = style;
+    pub fn style(mut self, style_sheet: &'a dyn StyleSheet) -> Self {
+        self.style_sheet = style_sheet;
         self
     }
 }
@@ -178,14 +178,22 @@ where
     fn draw(
         &self,
         renderer: &mut Renderer,
-        style: &renderer::Style,
+        renderer_style: &renderer::Style,
         layout: Layout<'_>,
         cursor_position: Point,
         viewport: &Rectangle,
     ) {
+        let style = self.style_sheet.style();
+
+        draw_background(renderer, &style, layout.bounds());
+
         self.content.draw(
             renderer,
-            style,
+            &renderer::Style {
+                text_color: style
+                    .text_color
+                    .unwrap_or(renderer_style.text_color),
+            },
             layout.children().next().unwrap(),
             cursor_position,
             viewport,
@@ -210,6 +218,26 @@ where
         layout: Layout<'_>,
     ) -> Option<overlay::Element<'_, Message, Renderer>> {
         self.content.overlay(layout.children().next().unwrap())
+    }
+}
+
+pub fn draw_background<Renderer>(
+    renderer: &mut Renderer,
+    style: &Style,
+    bounds: Rectangle,
+) where
+    Renderer: crate::Renderer,
+{
+    if style.background.is_some() || style.border_width > 0.0 {
+        renderer.fill_rectangle(renderer::Quad {
+            bounds,
+            background: style
+                .background
+                .unwrap_or(Background::Color(Color::TRANSPARENT)),
+            border_radius: style.border_radius,
+            border_width: style.border_width,
+            border_color: style.border_color,
+        });
     }
 }
 
