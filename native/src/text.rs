343f9b7e2e594bd1fef1ed511d71e81f9c44e3d9
Héctor Ramón Jiménez <hector0193@gmail.com>
2021-11-04 18:28:06 +0700

    Merge `iced_core::text` with `iced_native::text`

--- native/src/text.rs
+++ native/src/text.rs
@@ -1,7 +1,5 @@
 use crate::alignment;
-use crate::{Color, Point, Rectangle, Size};
-
-pub use iced_core::text::Hit;
+use crate::{Color, Point, Rectangle, Size, Vector};
 
 #[derive(Debug, Clone, Copy)]
 pub struct Text<'a, Font> {
@@ -12,6 +10,33 @@ pub struct Text<'a, Font> {
     pub font: Font,
     pub horizontal_alignment: alignment::Horizontal,
     pub vertical_alignment: alignment::Vertical,
+}
+
+/// The result of hit testing on text.
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Hit {
+    /// The point was within the bounds of the returned character index.
+    CharOffset(usize),
+    /// The provided point was not within the bounds of a glyph. The index
+    /// of the character with the closest centeroid position is returned,
+    /// as well as its delta.
+    NearestCharOffset(usize, Vector),
+}
+
+impl Hit {
+    /// Computes the cursor position corresponding to this [`HitTestResult`] .
+    pub fn cursor(self) -> usize {
+        match self {
+            Self::CharOffset(i) => i,
+            Self::NearestCharOffset(i, delta) => {
+                if delta.x > f32::EPSILON {
+                    i + 1
+                } else {
+                    i
+                }
+            }
+        }
+    }
 }
 
 pub trait Renderer: crate::Renderer {
