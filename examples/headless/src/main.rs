65400735fe02c3e860379e9e36eaf7931c527c08
James Connolly <jimmy@1024bees.com>
2022-01-01 19:19:24 -0500

    add first test

--- examples/headless/src/main.rs
+++ examples/headless/src/main.rs
@@ -4,6 +4,7 @@ use iced::{
 };
 
 use std::time::Duration;
+
 pub fn main() -> iced::Result {
     let msg_trace = vec![
         (Message::IncrementPressed, Duration::new(1, 0)),
@@ -92,5 +93,52 @@ impl Application for Counter {
                     .on_press(Message::TakeScreenshot),
             )
             .into()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    /// We start up the [`Counter`] applcation and take a screenshot. Then we compare with a golden
+    /// value
+    fn base_test() {
+        let msg_trace = vec![(Message::TakeScreenshot, Duration::new(1, 0))];
+        Counter::run_with_message_trace(
+            Settings {
+                headless: true,
+                window: iced::window::Settings {
+                    size: (600, 600),
+                    ..iced::window::Settings::default()
+                },
+
+                ..Settings::default()
+            },
+            msg_trace,
+        )
+        .expect("Running failed");
+
+
+        // get the golden png into a screenshot object
+        let path_to_base_golden =
+            format!("{}/golden/base_counter.png", env!("CARGO_MANIFEST_DIR"));
+
+        let golden = Screenshot::from_png(path_to_base_golden)
+            .expect("Png conversion failed for golden");
+
+
+        // get the png that is created by the application, put into a screenshot object
+        let screenshot_png =
+            format!("{}/counter_0.png", env!("CARGO_MANIFEST_DIR"));
+
+        let png_test =
+            Screenshot::from_png(screenshot_png.clone()).expect("png test");
+
+        // try to remove the screenshot, dont let this cause the test to fail though
+        let _ = std::fs::remove_file(screenshot_png);
+
+        //compare the golden object and the png test object
+        assert_eq!(golden, png_test);
     }
 }
