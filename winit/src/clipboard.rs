908259663de60ae62723a73c3e93159c4bd22586
Kai Mast <kaimast@cs.wisc.edu>
2022-01-27 11:27:12 -0600

    Remove wasm-specific clipboard

--- winit/src/clipboard.rs
+++ winit/src/clipboard.rs
@@ -3,43 +3,17 @@ pub use iced_native::clipboard::Action;
 
 use crate::command::{self, Command};
 
-/// A buffer for short-term storage and transfer within and between
-/// applications.
+#[allow(missing_docs)]
 #[allow(missing_debug_implementations)]
-#[cfg(target_arch = "wasm32")]
-pub struct Clipboard;
-
-#[cfg(target_arch = "wasm32")]
-impl Clipboard {
-    /// Creates a new [`Clipboard`] for the given window.
-    pub fn connect(_window: &winit::window::Window) -> Clipboard {
-        Clipboard
-    }
-
-    /// Reads the current content of the [`Clipboard`] as text.
-    pub fn read(&self) -> Option<String> {
-        None
-    }
-
-    /// Writes the given text contents to the [`Clipboard`].
-    pub fn write(&mut self, _contents: String) {}
-}
-
-/// A buffer for short-term storage and transfer within and between
-/// applications.
-#[allow(missing_debug_implementations)]
-#[cfg(not(target_arch = "wasm32"))]
 pub struct Clipboard {
     state: State,
 }
 
-#[cfg(not(target_arch = "wasm32"))]
 enum State {
     Connected(window_clipboard::Clipboard),
     Unavailable,
 }
 
-#[cfg(not(target_arch = "wasm32"))]
 impl Clipboard {
     /// Creates a new [`Clipboard`] for the given window.
     pub fn connect(window: &winit::window::Window) -> Clipboard {
