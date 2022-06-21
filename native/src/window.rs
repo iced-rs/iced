ec56c0686df1a200e37af951a3a8eca562c32a5c
Richard <richardsoncusto@gmail.com>
2022-06-21 15:59:45 -0300

    Introduce opaque `window::Id` type

--- native/src/window.rs
+++ native/src/window.rs
@@ -1,10 +1,12 @@
 //! Build window-based GUI applications.
 mod action;
 mod event;
+mod id;
 mod mode;
 mod user_attention;
 
 pub use action::Action;
 pub use event::Event;
+pub use id::Id;
 pub use mode::Mode;
 pub use user_attention::UserAttention;
