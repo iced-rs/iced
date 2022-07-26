6293a8abbdf2d335f56c19f3d0fa3765f2b5e3a1
Richard <richardsoncusto@gmail.com>
2022-07-26 16:46:12 -0300

    Allow closing the window from user code

--- native/src/window/id.rs
+++ native/src/window/id.rs
@@ -1,7 +1,7 @@
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 /// TODO(derezzedex)
 pub struct Id(u64);
 
