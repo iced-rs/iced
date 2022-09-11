09a531cd4427ff5dc972621094f33de0f94a0919
Ashley Wulber <ashley@system76.com>
2022-09-10 23:18:57 -0400

    fix: arc example

--- examples/arc/src/main.rs
+++ examples/arc/src/main.rs
@@ -80,7 +80,7 @@ impl<Message> canvas::Program<Message> for Arc {
         _cursor: Cursor,
     ) -> Vec<Geometry> {
         let geometry = self.cache.draw(bounds.size(), |frame| {
-            let palette = theme.palette();
+            let palette = theme.clone().palette();
 
             let center = frame.center();
             let radius = frame.width().min(frame.height()) / 5.0;
