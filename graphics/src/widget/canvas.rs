a8c3b2ddf7812d9466d6c7037ec921230d34683d
Héctor Ramón Jiménez <hector0193@gmail.com>
2022-11-03 05:59:13 +0100

    Fix documentation example of `Canvas`
    ... so it makes sense when seen from the `iced` crate.

--- graphics/src/widget/canvas.rs
+++ graphics/src/widget/canvas.rs
@@ -45,12 +45,16 @@ use std::marker::PhantomData;
 /// If you want to get a quick overview, here's how we can draw a simple circle:
 ///
 /// ```no_run
-/// // First, we define the data we need for drawing
-/// use iced_graphics::{Color, Rectangle};
-/// use iced_graphics::widget::Canvas;
-/// use iced_graphics::widget::canvas::{Cursor, Frame, Geometry, Path, Program};
-/// use iced_style::Theme;
+/// # mod iced {
+/// #     pub mod widget {
+/// #         pub use iced_graphics::widget::canvas;
+/// #     }
+/// #     pub use iced_native::{Color, Rectangle, Theme};
+/// # }
+/// use iced::widget::canvas::{self, Canvas, Cursor, Fill, Frame, Geometry, Path, Program};
+/// use iced::{Color, Rectangle, Theme};
 ///
+/// // First, we define the data we need for drawing
 /// #[derive(Debug)]
 /// struct Circle {
 ///     radius: f32,
