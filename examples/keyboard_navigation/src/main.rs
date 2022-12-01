bd890993c5441cdb1f99854de92e0a55b1c34bb5
Jason Aunkst <jaunkst@gmail.com>
2022-12-01 08:16:08 -0500

    keyboard events example cleanup

--- examples/keyboard_navigation/src/main.rs
+++ examples/keyboard_navigation/src/main.rs
@@ -1,6 +1,6 @@
 use iced::widget::{
-    button, checkbox, column, container, pick_list, radio, slider, text,
-    text_input, toggler, Row,
+    button, checkbox, container, pick_list, radio, slider, text, text_input,
+    toggler, Column, Row,
 };
 use iced::{executor, keyboard, subscription, Subscription};
 use iced::{Application, Command, Element, Length, Settings, Theme};
@@ -22,158 +22,6 @@ struct KeyboardNavigationDemo {
 enum ThemeType {
     Light,
     Dark,
-}
-
-#[derive(Debug, Clone)]
-enum Message {
-    ThemeChanged(ThemeType),
-    TextChanged(String),
-    TabPressed,
-    ButtonPressed,
-    CheckboxToggled(bool),
-    TogglerToggled(bool),
-    SliderChanged(f32),
-    LanguageSelected(Language),
-}
-
-impl Application for KeyboardNavigationDemo {
-    type Message = Message;
-    type Theme = Theme;
-    type Executor = executor::Default;
-    type Flags = ();
-
-    fn new(_flags: Self::Flags) -> (Self, Command<Message>) {
-        (
-            KeyboardNavigationDemo {
-                theme: Default::default(),
-                text_input_value: "Hello world!".to_string(),
-                checkbox_value: false,
-                toggler_value: false,
-                slider_value: 0.5,
-                selected_language: Language::ALL.first().cloned(),
-            },
-            Command::none(),
-        )
-    }
-
-    fn title(&self) -> String {
-        String::from("Keyboard Navigation - Iced")
-    }
-
-    fn subscription(&self) -> Subscription<Message> {
-        subscription::events_with(|event, status| match (event, status) {
-            (
-                iced::Event::Keyboard(keyboard::Event::KeyPressed {
-                    key_code: keyboard::KeyCode::Tab,
-                    modifiers: _,
-                    ..
-                }),
-                iced::event::Status::Ignored,
-            ) => Some(Message::TabPressed),
-            _ => None,
-        })
-    }
-
-    fn update(&mut self, message: Message) -> Command<Message> {
-        match message {
-            Message::ThemeChanged(theme) => {
-                self.theme = match theme {
-                    ThemeType::Light => Theme::Light,
-                    ThemeType::Dark => Theme::Dark,
-                };
-
-                Command::none()
-            }
-            Message::TextChanged(_) => Command::none(),
-            Message::ButtonPressed => {
-                println!("Button pressed!");
-                Command::none()
-            }
-            Message::CheckboxToggled(checked) => {
-                println!("Box toggled! {}", checked);
-                self.checkbox_value = checked;
-                Command::none()
-            }
-            Message::TogglerToggled(value) => {
-                println!("Toggler toggled! {}", value);
-                self.toggler_value = value;
-                Command::none()
-            }
-            Message::SliderChanged(value) => {
-                println!("Slider changed! {}", value);
-                self.slider_value = value;
-                Command::none()
-            }
-            Message::LanguageSelected(language) => {
-                println!("Language selected! {:?}", language);
-                self.selected_language = Some(language);
-                Command::none()
-            }
-            Message::TabPressed => iced::widget::focus_next(),
-        }
-    }
-
-    fn view(&self) -> Element<Message> {
-        let choose_theme = [ThemeType::Light, ThemeType::Dark].iter().fold(
-            column![text("Choose a theme:")].spacing(10),
-            |column, theme| {
-                column.push(radio(
-                    format!("{:?}", theme),
-                    *theme,
-                    Some(match self.theme {
-                        Theme::Light => ThemeType::Light,
-                        Theme::Dark => ThemeType::Dark,
-                        Theme::Custom(_) => ThemeType::Dark,
-                    }),
-                    Message::ThemeChanged,
-                ))
-            },
-        );
-
-        let pick_list = pick_list(
-            &Language::ALL[..],
-            self.selected_language,
-            Message::LanguageSelected,
-        )
-        .placeholder("Choose a language...");
-
-        let row_1 = Row::with_children(vec![choose_theme.into()])
-            .spacing(20)
-            .width(Length::Fill)
-            .height(Length::Fill);
-
-        let row_2 = Row::with_children(vec![
-            button("*").on_press(Message::ButtonPressed).into(),
-            checkbox("*", self.checkbox_value, Message::CheckboxToggled).into(),
-            text_input("*", &self.text_input_value, Message::TextChanged)
-                .into(),
-            toggler(
-                "*".to_owned(),
-                self.toggler_value,
-                Message::TogglerToggled,
-            )
-            .into(),
-            slider(0.0..=1.0, self.slider_value, Message::SliderChanged)
-                .step(0.25)
-                .into(),
-        ])
-        .spacing(20)
-        .width(Length::Fill)
-        .height(Length::Shrink);
-
-        let content = column![row_1, pick_list, row_2].spacing(20).padding(20);
-
-        container(content)
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .center_x()
-            .center_y()
-            .into()
-    }
-
-    fn theme(&self) -> Theme {
-        self.theme.clone()
-    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -220,5 +68,210 @@ impl std::fmt::Display for Language {
                 Language::Other => "Some other language",
             }
         )
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    ThemeChanged(ThemeType),
+    TextChanged(String),
+    TabPressed,
+    ButtonPressed,
+    CheckboxToggled(bool),
+    TogglerToggled(bool),
+    SliderChanged(f32),
+    LanguageSelected(Language),
+}
+
+impl Application for KeyboardNavigationDemo {
+    type Message = Message;
+    type Theme = Theme;
+    type Executor = executor::Default;
+    type Flags = ();
+
+    fn new(_flags: Self::Flags) -> (Self, Command<Message>) {
+        (
+            KeyboardNavigationDemo {
+                theme: Default::default(),
+                text_input_value: "Text Input".to_string(),
+                checkbox_value: false,
+                toggler_value: false,
+                slider_value: 0.5,
+                selected_language: Language::ALL.first().cloned(),
+            },
+            Command::none(),
+        )
+    }
+
+    fn title(&self) -> String {
+        String::from("Keyboard Navigation - Iced")
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        subscription::events_with(|event, status| match (event, status) {
+            (
+                iced::Event::Keyboard(keyboard::Event::KeyPressed {
+                    key_code: keyboard::KeyCode::Tab,
+                    modifiers: _,
+                    ..
+                }),
+                iced::event::Status::Ignored,
+            ) => Some(Message::TabPressed),
+            _ => None,
+        })
+    }
+
+    fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            Message::ThemeChanged(theme) => {
+                println!("Radio changed!");
+                self.theme = match theme {
+                    ThemeType::Light => Theme::Light,
+                    ThemeType::Dark => Theme::Dark,
+                };
+
+                Command::none()
+            }
+            Message::TextChanged(_) => Command::none(),
+            Message::ButtonPressed => {
+                println!("Button pressed!");
+                Command::none()
+            }
+            Message::CheckboxToggled(checked) => {
+                println!("Box toggled! {}", checked);
+                self.checkbox_value = checked;
+                Command::none()
+            }
+            Message::TogglerToggled(value) => {
+                println!("Toggler toggled! {}", value);
+                self.toggler_value = value;
+                Command::none()
+            }
+            Message::SliderChanged(value) => {
+                println!("Slider changed! {}", value);
+                self.slider_value = value;
+                Command::none()
+            }
+            Message::LanguageSelected(language) => {
+                println!("Language selected! {:?}", language);
+                self.selected_language = Some(language);
+                Command::none()
+            }
+            Message::TabPressed => iced::widget::focus_next(),
+        }
+    }
+
+    fn view(&self) -> Element<Message> {
+        let radio_example = container(
+            Column::with_children(vec![[ThemeType::Light, ThemeType::Dark]
+                .iter()
+                .fold(
+                    Column::with_children(vec![text("Radio").into()])
+                        .spacing(10),
+                    |column, theme| {
+                        column.push(radio(
+                            format!("{:?}", theme),
+                            *theme,
+                            Some(match self.theme {
+                                Theme::Light => ThemeType::Light,
+                                Theme::Dark => ThemeType::Dark,
+                                Theme::Custom(_) => ThemeType::Dark,
+                            }),
+                            Message::ThemeChanged,
+                        ))
+                    },
+                )
+                .into()])
+            .width(Length::Fill)
+            .height(Length::Fill),
+        )
+        .into();
+
+        let list_picker_example = container(
+            Column::with_children(vec![
+                text("List Picker").into(),
+                pick_list(
+                    &Language::ALL[..],
+                    self.selected_language,
+                    Message::LanguageSelected,
+                )
+                .placeholder("Choose a language...")
+                .into(),
+            ])
+            .width(Length::Fill)
+            .height(Length::Fill),
+        )
+        .into();
+
+        let button_example = container(
+            Column::with_children(vec![button("Button")
+                .on_press(Message::ButtonPressed)
+                .into()])
+            .width(Length::Fill)
+            .height(Length::Fill),
+        )
+        .into();
+
+        let text_input_example = container(
+            Column::with_children(vec![text_input(
+                "Text Input",
+                &self.text_input_value,
+                Message::TextChanged,
+            )
+            .into()])
+            .width(Length::Fill)
+            .height(Length::Fill),
+        )
+        .into();
+
+        let toggler_example = container(
+            Column::with_children(vec![
+                text("Toggler").into(),
+                toggler(
+                    "Toggle".to_owned(),
+                    self.toggler_value,
+                    Message::TogglerToggled,
+                )
+                .width(Length::Units(35))
+                .into(),
+            ])
+            .width(Length::Fill)
+            .height(Length::Fill),
+        )
+        .into();
+
+        let slider_example = container(
+            Column::with_children(vec![
+                text("Slider").into(),
+                text("Enter or Space to toggle grab").into(),
+                slider(0.0..=1.0, self.slider_value, Message::SliderChanged)
+                    .step(0.25)
+                    .into()
+            ])
+            .width(Length::Fill)
+            .height(Length::Fill),
+        )
+        .into();
+
+        let content = Column::with_children(vec![
+            radio_example,
+            list_picker_example,
+            button_example,
+            text_input_example,
+            toggler_example,
+            slider_example
+        ])
+        .spacing(20)
+        .padding(20)
+        .width(Length::Units(250));
+
+        container(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .into()
+    }
+
+    fn theme(&self) -> Theme {
+        self.theme.clone()
     }
 }
