b2a9a1e73cb7b2026b2eeaa2be2c04a61c5efb21
Bingus <shankern@protonmail.com>
2023-03-02 08:31:39 -0800

    Fixed fullscreen only being possible on primary monitor.

--- winit/src/application.rs
+++ winit/src/application.rs
@@ -762,7 +762,7 @@ pub fn run_command<A, E>(
                 window::Action::ChangeMode(mode) => {
                     window.set_visible(conversion::visible(mode));
                     window.set_fullscreen(conversion::fullscreen(
-                        window.primary_monitor(),
+                        window.current_monitor(),
                         mode,
                     ));
                 }
