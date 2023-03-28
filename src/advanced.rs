2e236869f5f9297d40d66d5d9e3a82324b8617f2
Héctor Ramón Jiménez <hector0193@gmail.com>
2023-03-29 00:17:12 +0200

    Export `iced_core::mouse` in `advanced` module

--- src/advanced.rs
+++ src/advanced.rs
@@ -1,6 +1,7 @@
 //! Leverage advanced concepts like custom widgets.
 pub use crate::core::image;
 pub use crate::core::layout::{self, Layout};
+pub use crate::core::mouse;
 pub use crate::core::overlay::{self, Overlay};
 pub use crate::core::renderer::{self, Renderer};
 pub use crate::core::svg;
