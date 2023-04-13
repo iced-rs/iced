db4b899fd215f0607474006a9aef0361e74d481f
Héctor Ramón Jiménez <hector0193@gmail.com>
2023-04-13 06:03:44 +0200

    Fix Wasm target for `integration_wgpu`

--- examples/integration_wgpu/src/main.rs
+++ examples/integration_wgpu/src/main.rs
@@ -33,7 +33,8 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
         web_sys::window()
             .and_then(|win| win.document())
             .and_then(|doc| doc.get_element_by_id("iced_canvas"))
-            .and_then(|element| element.dyn_into::<HtmlCanvasElement>().ok())?
+            .and_then(|element| element.dyn_into::<HtmlCanvasElement>().ok())
+            .expect("Canvas with id `iced_canvas` is missing")
     };
     #[cfg(not(target_arch = "wasm32"))]
     env_logger::init();
