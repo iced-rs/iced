0cc85c7820136f3ad858c287e8e35884604e7bd0
Austin M. Reppert <austinmreppert@gmail.com>
2023-06-29 02:14:21 -0400

    Add touch support for toggler. (#1935)
    * Add touch support for toggler.
    
    * Fix formatting.
    
    * Fix consistency of imports in `iced_widget::toggler`
    
    ---------
    
    Co-authored-by: Austin M. Reppert <austinmreppert@protonmail.com>
    Co-authored-by: Héctor Ramón Jiménez <hector0193@gmail.com>

--- widget/src/toggler.rs
+++ widget/src/toggler.rs
@@ -5,6 +5,7 @@ use crate::core::layout;
 use crate::core::mouse;
 use crate::core::renderer;
 use crate::core::text;
+use crate::core::touch;
 use crate::core::widget::Tree;
 use crate::core::{
     Alignment, Clipboard, Element, Event, Layout, Length, Pixels, Rectangle,
@@ -208,7 +209,8 @@ where
         shell: &mut Shell<'_, Message>,
     ) -> event::Status {
         match event {
-            Event::Mouse(mouse::Event::ButtonPressed(mouse::Button::Left)) => {
+            Event::Mouse(mouse::Event::ButtonPressed(mouse::Button::Left))
+            | Event::Touch(touch::Event::FingerPressed { .. }) => {
                 let mouse_over = cursor.is_over(layout.bounds());
 
                 if mouse_over {
