fbc9ef74c406b2a58bb2a093262323e346faf485
Thomas <thomasbirke@gmx.de>
2023-09-09 22:42:41 +0200

    Change init_with_level to init in examples

--- examples/tour/src/main.rs
+++ examples/tour/src/main.rs
@@ -10,7 +10,7 @@ use iced::{Color, Element, Font, Length, Renderer, Sandbox, Settings};
 pub fn main() -> iced::Result {
     #[cfg(target_arch = "wasm32")]
     {
-        console_log::init_with_level(log::Level::Debug).expect("Initialize logger");
+        console_log::init().expect("Initialize logger");
         std::panic::set_hook(Box::new(console_error_panic_hook::hook));
     }
 
