b8ddd158da1b4e73e67fd090f8d36ed07f191874
Héctor Ramón Jiménez <hector@hecrj.dev>
2023-09-20 16:27:54 +0200

    Simplify `map` call in `iced_wgpu::triangle`

--- wgpu/src/triangle.rs
+++ wgpu/src/triangle.rs
@@ -349,7 +349,7 @@ fn multisample_state(
     antialiasing: Option<Antialiasing>,
 ) -> wgpu::MultisampleState {
     wgpu::MultisampleState {
-        count: antialiasing.map(|a| a.sample_count()).unwrap_or(1),
+        count: antialiasing.map(Antialiasing::sample_count).unwrap_or(1),
         mask: !0,
         alpha_to_coverage_enabled: false,
     }
