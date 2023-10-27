86b877517feb15b2155c6cfef29246a3f281c8ae
Héctor Ramón Jiménez <hector@hecrj.dev>
2023-10-27 03:21:40 +0200

    Update `wgpu` to `0.18` and `cosmic-text` to `0.10`

--- wgpu/src/backend.rs
+++ wgpu/src/backend.rs
@@ -222,10 +222,12 @@ impl Backend {
                             }),
                             None => wgpu::LoadOp::Load,
                         },
-                        store: true,
+                        store: wgpu::StoreOp::Store,
                     },
                 })],
                 depth_stencil_attachment: None,
+                timestamp_writes: None,
+                occlusion_query_set: None,
             },
         ));
 
@@ -271,11 +273,13 @@ impl Backend {
                                 resolve_target: None,
                                 ops: wgpu::Operations {
                                     load: wgpu::LoadOp::Load,
-                                    store: true,
+                                    store: wgpu::StoreOp::Store,
                                 },
                             },
                         )],
                         depth_stencil_attachment: None,
+                        timestamp_writes: None,
+                        occlusion_query_set: None,
                     },
                 ));
             }
