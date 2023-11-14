8f384c83be242f9318685530ee52dd6c27b2bb62
Héctor Ramón Jiménez <hector@hecrj.dev>
2023-11-14 15:54:10 +0100

    Remove unsused `custom.rs` file in `iced_wgpu`

--- wgpu/src/custom.rs
+++
@@ -1,8 +0,0 @@
-//! Draw custom primitives.
-use crate::core::{Rectangle, Size};
-use crate::graphics::Transformation;
-use crate::primitive;
-
-use std::any::{Any, TypeId};
-use std::collections::HashMap;
-use std::fmt::Debug;
