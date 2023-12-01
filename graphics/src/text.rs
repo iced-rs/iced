99899d49cc93cdec3832f7b5ecad867fdd421e07
Héctor Ramón Jiménez <hector@hecrj.dev>
2023-12-01 15:04:08 +0100

    Clamp `text::measure` to `Buffer::size`

--- graphics/src/text.rs
+++ graphics/src/text.rs
@@ -76,7 +76,12 @@ pub fn measure(buffer: &cosmic_text::Buffer) -> Size {
             (run.line_w.max(width), total_lines + 1)
         });
 
-    Size::new(width, total_lines as f32 * buffer.metrics().line_height)
+    let (max_width, max_height) = buffer.size();
+
+    Size::new(
+        width.min(max_width),
+        (total_lines as f32 * buffer.metrics().line_height).min(max_height),
+    )
 }
 
 /// Returns the attributes of the given [`Font`].
