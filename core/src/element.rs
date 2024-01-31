b10a27abeeb3a1dfb2cde3b649cc2a53f1d034e9
Cory Forsstrom <cforsstrom18@gmail.com>
2024-01-31 14:39:51 -0800

    Call size_hint from Explain

--- core/src/element.rs
+++ core/src/element.rs
@@ -481,6 +481,10 @@ where
         self.element.widget.size()
     }
 
+    fn size_hint(&self) -> Size<Length> {
+        self.element.widget.size_hint()
+    }
+
     fn tag(&self) -> tree::Tag {
         self.element.widget.tag()
     }
