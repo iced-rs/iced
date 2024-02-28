1fda2d151d97d80806bce85cf77bbd2b5c9f61a3
Héctor Ramón Jiménez <hector@hecrj.dev>
2024-02-28 19:38:08 +0100

    Implement `Display` for `Timing` in `iced_sentinel`

--- sentinel/src/timing.rs
+++ sentinel/src/timing.rs
@@ -2,6 +2,7 @@ use crate::core::time::{Duration, SystemTime};
 use crate::core::window;
 
 use serde::{Deserialize, Serialize};
+use std::fmt;
 
 #[derive(
     Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize,
@@ -24,4 +25,19 @@ pub enum Stage {
     Draw(window::Id),
     Render(window::Id),
     Custom(window::Id, String),
+}
+
+impl fmt::Display for Stage {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Boot => write!(f, "Boot"),
+            Self::Update => write!(f, "Update"),
+            Self::View(_) => write!(f, "View"),
+            Self::Layout(_) => write!(f, "Layout"),
+            Self::Interact(_) => write!(f, "Interact"),
+            Self::Draw(_) => write!(f, "Draw"),
+            Self::Render(_) => write!(f, "Render"),
+            Self::Custom(_, name) => f.write_str(name),
+        }
+    }
 }
