7f13fab0582fe681b8546126245512bdc2338ead
Héctor Ramón Jiménez <hector@hecrj.dev>
2024-06-14 03:15:14 +0200

    Use all themes in `multi_window` example

--- examples/multi_window/src/main.rs
+++ examples/multi_window/src/main.rs
@@ -160,11 +160,7 @@ impl Window {
             title: format!("Window_{}", count),
             scale_input: "1.0".to_string(),
             current_scale: 1.0,
-            theme: if count % 2 == 0 {
-                Theme::Light
-            } else {
-                Theme::Dark
-            },
+            theme: Theme::ALL[count % Theme::ALL.len()].clone(),
             input_id: text_input::Id::unique(),
         }
     }
