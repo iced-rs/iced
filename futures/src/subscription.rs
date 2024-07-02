2b19471d1cfe4cf034b026aa6620b1685a5ab772
Héctor Ramón Jiménez <hector@hecrj.dev>
2024-07-02 19:01:04 +0200

    Simplify `subscription::channel` example

--- futures/src/subscription.rs
+++ futures/src/subscription.rs
@@ -369,44 +369,29 @@ where
 ///     // ...
 /// }
 ///
-/// enum State {
-///     Starting,
-///     Ready(mpsc::Receiver<Input>),
-/// }
-///
 /// fn some_worker() -> Subscription<Event> {
 ///     struct SomeWorker;
 ///
 ///     subscription::channel(std::any::TypeId::of::<SomeWorker>(), 100, |mut output| async move {
-///         let mut state = State::Starting;
+///         // Create channel
+///         let (sender, mut receiver) = mpsc::channel(100);
+///
+///         // Send the sender back to the application
+///         output.send(Event::Ready(sender)).await;
 ///
 ///         loop {
-///             match &mut state {
-///                 State::Starting => {
-///                     // Create channel
-///                     let (sender, receiver) = mpsc::channel(100);
+///             use iced_futures::futures::StreamExt;
 ///
-///                     // Send the sender back to the application
-///                     output.send(Event::Ready(sender)).await;
+///             // Read next input sent from `Application`
+///             let input = receiver.select_next_some().await;
 ///
-///                     // We are ready to receive messages
-///                     state = State::Ready(receiver);
-///                 }
-///                 State::Ready(receiver) => {
-///                     use iced_futures::futures::StreamExt;
+///             match input {
+///                 Input::DoSomeWork => {
+///                     // Do some async work...
 ///
-///                     // Read next input sent from `Application`
-///                     let input = receiver.select_next_some().await;
-///
-///                     match input {
-///                         Input::DoSomeWork => {
-///                             // Do some async work...
-///
-///                             // Finally, we can optionally produce a message to tell the
-///                             // `Application` the work is done
-///                             output.send(Event::WorkFinished).await;
-///                         }
-///                     }
+///                     // Finally, we can optionally produce a message to tell the
+///                     // `Application` the work is done
+///                     output.send(Event::WorkFinished).await;
 ///                 }
 ///             }
 ///         }
