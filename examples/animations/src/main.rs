93622c0b58f3385e77db96a5b7c39c860a43a931
LazyTanuki <43273245+lazytanuki@users.noreply.github.com>
2024-07-07 13:23:35 +0200

    wip: add scrollable to animations example

--- examples/animations/src/main.rs
+++ examples/animations/src/main.rs
@@ -1,19 +1,21 @@
 use iced::{
     widget::{
-        button, checkbox, column, container, radio, row, text_input, Toggler,
+        button, checkbox, column, container, row, scrollable, text, text_input,
+        Toggler,
     },
-    Element,
+    Element, Length, Task, Theme,
 };
+use once_cell::sync::Lazy;
+use std::fmt::Write;
 
 pub fn main() -> iced::Result {
-    iced::run("Animated widgets", Animations::update, Animations::view)
+    iced::application("Animated widgets", Animations::update, Animations::view)
+        .theme(Animations::theme)
+        .run()
 }
 
 #[derive(Default)]
 struct Animations {
-    _animation_multiplier: i32,
-    radio1: Option<usize>,
-    radio2: Option<usize>,
     input_text: String,
     toggled: bool,
     checked: bool,
@@ -23,35 +25,54 @@ struct Animations {
 #[derive(Debug, Clone)]
 enum Message {
     ButtonPressed,
-    RadioPressed(usize),
     TextSubmitted,
     TextChanged(String),
     Toggle(bool),
     Check(bool),
     DisableAnimations(bool),
+    GoToStart,
+    GoToEnd,
 }
 
+static SCROLLABLE_TEXT: Lazy<String> = Lazy::new(|| {
+    (0..50).fold(String::new(), |mut output, i| {
+        let _ = writeln!(
+            output,
+            "This is a long text string to test the scrollbar: {i}\n"
+        );
+        output
+    })
+});
+static SCROLLABLE_ID: Lazy<scrollable::Id> = Lazy::new(scrollable::Id::unique);
+
 impl Animations {
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Message) -> Task<Message> {
         match message {
-            Message::RadioPressed(i) => match i {
-                1 => {
-                    self.radio1 = Some(1);
-                    self.radio2 = None;
-                }
-                2 => {
-                    self.radio2 = Some(2);
-                    self.radio1 = None;
-                }
-                _ => {}
-            },
             Message::TextChanged(txt) => {
                 self.input_text = txt;
+                Task::none()
             }
-            Message::Toggle(t) => self.toggled = t,
-            Message::TextSubmitted | Message::ButtonPressed => {}
-            Message::Check(c) => self.checked = c,
-            Message::DisableAnimations(b) => self.animations_disabled = b,
+            Message::Toggle(t) => {
+                self.toggled = t;
+                Task::none()
+            }
+            Message::TextSubmitted | Message::ButtonPressed => Task::none(),
+            Message::Check(c) => {
+                self.checked = c;
+                Task::none()
+            }
+            Message::DisableAnimations(b) => {
+                self.animations_disabled = b;
+                Task::none()
+            }
+            Message::GoToStart => scrollable::snap_to(
+                SCROLLABLE_ID.clone(),
+                scrollable::RelativeOffset::START,
+            ),
+            Message::GoToEnd => scrollable::snap_to(
+                SCROLLABLE_ID.clone(),
+                scrollable::RelativeOffset::END,
+            ),
         }
     }
 
@@ -87,21 +108,29 @@ impl Animations {
             button("Text")
                 .on_press(Message::ButtonPressed)
                 .style(button::text),
-            radio("Click me 1", 1, self.radio1, |i| {
-                Message::RadioPressed(i)
-            }),
-            radio("Click me 2", 2, self.radio2, |i| {
-                Message::RadioPressed(i)
-            }),
             checkbox("Check me", self.checked)
                 .on_toggle(|t| { Message::Check(t) }),
             Toggler::new(Some("Toggle me".into()), self.toggled, |t| {
                 Message::Toggle(t)
             }),
+            scrollable(
+                container(column![
+                    button("Go to end").on_press(Message::GoToEnd),
+                    text(SCROLLABLE_TEXT.as_str()),
+                    button("Go to start").on_press(Message::GoToStart)
+                ])
+                .width(Length::Fill)
+                .padding(5)
+            )
+            .id(SCROLLABLE_ID.clone())
         ]
         .spacing(10)
         .padding(50)
         .max_width(800)
         .into()
+    }
+
+    fn theme(&self) -> Theme {
+        Theme::Light
     }
 }
