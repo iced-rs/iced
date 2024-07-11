4ce2a207a6c2f28345dc86804a12f2faab3d07ab
Héctor Ramón Jiménez <hector@hecrj.dev>
2024-07-11 04:08:40 +0200

    Introduce `stream::try_channel` helper

--- futures/src/stream.rs
+++ futures/src/stream.rs
@@ -23,3 +23,24 @@ where
 
     stream::select(receiver, runner)
 }
+
+/// Creates a new [`Stream`] that produces the items sent from a [`Future`]
+/// that can fail to the [`mpsc::Sender`] provided to the closure.
+pub fn try_channel<T, E, F>(
+    size: usize,
+    f: impl FnOnce(mpsc::Sender<T>) -> F,
+) -> impl Stream<Item = Result<T, E>>
+where
+    F: Future<Output = Result<(), E>>,
+{
+    let (sender, receiver) = mpsc::channel(size);
+
+    let runner = stream::once(f(sender)).filter_map(|result| async {
+        match result {
+            Ok(()) => None,
+            Err(error) => Some(Err(error)),
+        }
+    });
+
+    stream::select(receiver.map(Ok), runner)
+}
