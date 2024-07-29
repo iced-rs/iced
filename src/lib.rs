57a9702ee8fdaf108b6d7f344bbc21e645f68be1
Wail Abou <abou.w@hotmail.com>
2024-07-29 23:36:01 +0200

    Shortcut export for Fixed

--- src/lib.rs
+++ src/lib.rs
@@ -490,7 +490,7 @@ pub use iced_futures::Subscription;
 pub use alignment::Horizontal::{Left, Right};
 pub use alignment::Vertical::{Bottom, Top};
 pub use Alignment::Center;
-pub use Length::{Fill, FillPortion, Shrink};
+pub use Length::{Fill, FillPortion, Shrink, Fixed};
 
 pub mod task {
     //! Create runtime tasks.
