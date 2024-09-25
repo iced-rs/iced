a949e59f3e2125e33ce7ebe904d6a98711c1fedf
Tommy Volk <tvolk131@gmail.com>
2024-09-24 20:57:53 -0500

    feat: set total size of QRCode

--- examples/qr_code/src/main.rs
+++ examples/qr_code/src/main.rs
@@ -1,5 +1,11 @@
-use iced::widget::{center, column, pick_list, qr_code, row, text, text_input};
+use iced::widget::{
+    center, column, pick_list, qr_code, row, slider, text, text_input, toggler,
+};
 use iced::{Center, Element, Theme};
+
+const QR_CODE_EXACT_SIZE_MIN_PX: u32 = 200;
+const QR_CODE_EXACT_SIZE_MAX_PX: u32 = 400;
+const QR_CODE_EXACT_SIZE_SLIDER_STEPS: u8 = 100;
 
 pub fn main() -> iced::Result {
     iced::application(
@@ -15,12 +21,16 @@ pub fn main() -> iced::Result {
 struct QRGenerator {
     data: String,
     qr_code: Option<qr_code::Data>,
+    display_with_fixed_size: bool,
+    fixed_size_slider_value: u8,
     theme: Theme,
 }
 
 #[derive(Debug, Clone)]
 enum Message {
     DataChanged(String),
+    SetDisplayWithFixedSize(bool),
+    FixedSizeSliderChanged(u8),
     ThemeChanged(Theme),
 }
 
@@ -37,6 +47,12 @@ impl QRGenerator {
                 };
 
                 self.data = data;
+            }
+            Message::SetDisplayWithFixedSize(exact_size) => {
+                self.display_with_fixed_size = exact_size;
+            }
+            Message::FixedSizeSliderChanged(value) => {
+                self.fixed_size_slider_value = value;
             }
             Message::ThemeChanged(theme) => {
                 self.theme = theme;
@@ -61,11 +77,33 @@ impl QRGenerator {
         .align_y(Center);
 
         let content = column![title, input, choose_theme]
-            .push_maybe(
-                self.qr_code
-                    .as_ref()
-                    .map(|data| qr_code(data).cell_size(10)),
+            .push(
+                toggler(self.display_with_fixed_size)
+                    .on_toggle(Message::SetDisplayWithFixedSize)
+                    .label("Fixed Size"),
             )
+            .push_maybe(self.display_with_fixed_size.then(|| {
+                slider(
+                    1..=QR_CODE_EXACT_SIZE_SLIDER_STEPS,
+                    self.fixed_size_slider_value,
+                    Message::FixedSizeSliderChanged,
+                )
+            }))
+            .push_maybe(self.qr_code.as_ref().map(|data| {
+                if self.display_with_fixed_size {
+                    // Convert the slider value to a size in pixels.
+                    let qr_code_size_px = (self.fixed_size_slider_value as f32
+                        / QR_CODE_EXACT_SIZE_SLIDER_STEPS as f32)
+                        * (QR_CODE_EXACT_SIZE_MAX_PX
+                            - QR_CODE_EXACT_SIZE_MIN_PX)
+                            as f32
+                        + QR_CODE_EXACT_SIZE_MIN_PX as f32;
+
+                    qr_code(data).total_size(qr_code_size_px)
+                } else {
+                    qr_code(data).cell_size(10.0)
+                }
+            }))
             .width(700)
             .spacing(20)
             .align_x(Center);
