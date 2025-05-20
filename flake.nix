0214971f54334320362a5fb19be2873449ebff1e
Sebastiano Giordano <46520354+Krahos@users.noreply.github.com>
2025-05-20 17:07:30 +0200

    chore: adding support for nix devshell and direnv

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Rust devshell for Iced";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell rec {
+          nativeBuildInputs = [
+            bacon
+            cargo-udeps
+            cargo-edit
+            rust-analyzer
+            cargo-deny
+            rust-bin.stable.latest.default
+
+            pkg-config
+            dbus
+          ];
+          buildInputs = [
+            libxkbcommon
+          ];
+          runtimeDependencies = [
+            wayland
+          ];
+          LD_LIBRARY_PATH = lib.makeLibraryPath (nativeBuildInputs ++ buildInputs ++ runtimeDependencies);
+        };
+      }
+    );
+}
