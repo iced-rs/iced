83a7a50d8b85ecac2e77bca43fc3a08d486d7e69
Héctor Ramón Jiménez <hector@hecrj.dev>
2025-08-02 20:52:17 +0200

    Add `weak` styling for `rule`

--- widget/src/rule.rs
+++ widget/src/rule.rs
@@ -300,3 +300,15 @@ pub fn default(theme: &Theme) -> Style {
         snap: true,
     }
 }
+
+/// A [`Rule`] styling using the weak background color.
+pub fn weak(theme: &Theme) -> Style {
+    let palette = theme.extended_palette();
+
+    Style {
+        color: palette.background.weak.color,
+        radius: 0.0.into(),
+        fill_mode: FillMode::Full,
+        snap: true,
+    }
+}
