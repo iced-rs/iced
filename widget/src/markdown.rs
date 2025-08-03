6b937fb6e122dc3b4e1704311a14a58a05ec693a
Héctor Ramón Jiménez <hector@hecrj.dev>
2025-08-03 03:02:45 +0200

    Use smart horizontal `scrollable` in `markdown::table`

--- widget/src/markdown.rs
+++ widget/src/markdown.rs
@@ -1430,7 +1430,7 @@ where
 {
     use crate::table;
 
-    table(
+    let table = table(
         columns.iter().enumerate().map(move |(i, column)| {
             table::column(
                 items(viewer, settings, &column.header),
@@ -1459,8 +1459,14 @@ where
     )
     .padding_x(settings.spacing.0)
     .padding_y(settings.spacing.0 / 2.0)
-    .separator_x(0)
-    .into()
+    .separator_x(0);
+
+    scrollable(table)
+        .direction(scrollable::Direction::Horizontal(
+            scrollable::Scrollbar::default(),
+        ))
+        .spacing(settings.spacing.0 / 2.0)
+        .into()
 }
 
 /// Displays a column of items with the default look.
