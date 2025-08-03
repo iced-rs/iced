c98f3d525a78b45fea6e218c42c92a1002710708
Héctor Ramón Jiménez <hector@hecrj.dev>
2025-08-03 21:09:15 +0200

    Add `background` styling for `button`

--- widget/src/button.rs
+++ widget/src/button.rs
@@ -688,7 +688,28 @@ pub fn text(theme: &Theme, status: Status) -> Style {
     }
 }
 
-/// A subtle button using background shades.
+/// A button using background shades.
+pub fn background(theme: &Theme, status: Status) -> Style {
+    let palette = theme.extended_palette();
+    let base = styled(palette.background.base);
+
+    match status {
+        Status::Active => base,
+        Status::Pressed => Style {
+            background: Some(Background::Color(
+                palette.background.strong.color,
+            )),
+            ..base
+        },
+        Status::Hovered => Style {
+            background: Some(Background::Color(palette.background.weak.color)),
+            ..base
+        },
+        Status::Disabled => disabled(base),
+    }
+}
+
+/// A subtle button using weak background shades.
 pub fn subtle(theme: &Theme, status: Status) -> Style {
     let palette = theme.extended_palette();
     let base = styled(palette.background.weakest);
