f9a6a3051d0a5e1c9f47ebe4071daf5621f8d9c1
Héctor Ramón Jiménez <hector@hecrj.dev>
2025-08-23 21:13:54 +0200

    Remove Oklab gradient support

--- wgpu/src/triangle.rs
+++ wgpu/src/triangle.rs
@@ -827,7 +827,6 @@ mod solid {
 mod gradient {
     use crate::Buffer;
     use crate::graphics::Antialiasing;
-    use crate::graphics::color;
     use crate::graphics::mesh;
     use crate::triangle;
 
@@ -922,35 +921,15 @@ mod gradient {
                 device.create_shader_module(wgpu::ShaderModuleDescriptor {
                     label: Some("iced_wgpu.triangle.gradient.shader"),
                     source: wgpu::ShaderSource::Wgsl(
-                        std::borrow::Cow::Borrowed(
-                            if color::GAMMA_CORRECTION {
-                                concat!(
-                                    include_str!("shader/triangle.wgsl"),
-                                    "\n",
-                                    include_str!(
-                                        "shader/triangle/gradient.wgsl"
-                                    ),
-                                    "\n",
-                                    include_str!("shader/color.wgsl"),
-                                    "\n",
-                                    include_str!("shader/color/oklab.wgsl")
-                                )
-                            } else {
-                                concat!(
-                                    include_str!("shader/triangle.wgsl"),
-                                    "\n",
-                                    include_str!(
-                                        "shader/triangle/gradient.wgsl"
-                                    ),
-                                    "\n",
-                                    include_str!("shader/color.wgsl"),
-                                    "\n",
-                                    include_str!(
-                                        "shader/color/linear_rgb.wgsl"
-                                    )
-                                )
-                            },
-                        ),
+                        std::borrow::Cow::Borrowed(concat!(
+                            include_str!("shader/triangle.wgsl"),
+                            "\n",
+                            include_str!("shader/triangle/gradient.wgsl"),
+                            "\n",
+                            include_str!("shader/color.wgsl"),
+                            "\n",
+                            include_str!("shader/color/linear_rgb.wgsl")
+                        )),
                     ),
                 });
 
