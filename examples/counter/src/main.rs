ba8b739f84e16f345eb756d65dd9009d2e671d7b
Alexandre Viau <alexandre@alexandreviau.ca>
2025-10-16 13:30:09 -0400

    examples/counter: add title to run()

--- examples/counter/src/main.rs
+++ examples/counter/src/main.rs
@@ -2,7 +2,7 @@ use iced::Center;
 use iced::widget::{Column, button, column, text};
 
 pub fn main() -> iced::Result {
-    iced::run(Counter::update, Counter::view)
+    iced::run("Counter App", Counter::update, Counter::view)
 }
 
 #[derive(Default)]
