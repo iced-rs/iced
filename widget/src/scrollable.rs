542de5aa43cf39fab7c73b193157dc54f067c457
Héctor Ramón Jiménez <hector@hecrj.dev>
2025-12-11 15:02:12 +0100

    Hint `scrollable` translation for better pixel alignment

--- widget/src/scrollable.rs
+++ widget/src/scrollable.rs
@@ -1073,6 +1073,9 @@ where
 
         // Draw inner content
         if scrollbars.active() {
+            let scale_factor = renderer.scale_factor().unwrap_or(1.0);
+            let translation = (translation * scale_factor).round() / scale_factor;
+
             renderer.with_layer(visible_bounds, |renderer| {
                 renderer.with_translation(
                     Vector::new(-translation.x, -translation.y),
