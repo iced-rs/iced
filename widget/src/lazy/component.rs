3a875d560b1610812eb968f48ec36dacd4af19c8
Piotr Siuszko <siuszko@zoho.com>
2026-02-11 18:50:50 +0100

    Update component.rs

--- widget/src/lazy/component.rs
+++ widget/src/lazy/component.rs
@@ -330,6 +330,7 @@ where
         local_shell.revalidate_layout(|| shell.invalidate_layout());
         shell.request_redraw_at(local_shell.redraw_request());
         shell.request_input_method(local_shell.input_method());
+        #[cfg(feature = "clipboard")]
         shell.clipboard_mut().merge(local_shell.clipboard_mut());
 
         if !local_messages.is_empty() {
@@ -597,6 +598,7 @@ where
         local_shell.revalidate_layout(|| shell.invalidate_layout());
         shell.request_redraw_at(local_shell.redraw_request());
         shell.request_input_method(local_shell.input_method());
+        #[cfg(feature = "clipboard")]
         shell.clipboard_mut().merge(local_shell.clipboard_mut());
 
         if !local_messages.is_empty() {
