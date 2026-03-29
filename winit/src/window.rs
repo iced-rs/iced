8955d9cc377b60e2a89822433c01aa0eb6b92995
KENZ <KENZ.gelsoft@gmail.com>
2026-03-30 06:19:21 +0900

    chore: improve comment and code to explain clearer why we workaround on linux-like

--- winit/src/window.rs
+++ winit/src/window.rs
@@ -266,14 +266,16 @@ where
         }
 
         if self.ime_state != Some((cursor, purpose)) {
-            let (cursor_y, cursor_height) = if cfg!(any(target_os = "windows", target_os = "macos"))
-            {
-                (cursor.y, cursor.height)
-            } else {
-                // Specify the bottom-left position of the cursor because
-                // only the position is supported on Linux (or other platforms).
-                (cursor.y + cursor.height, 0f32)
-            };
+            // Specify only the bottom-left position of the cursor on Linux
+            // because fcitx5 doesn't work well with cursor areas of
+            // the top-left position and height.
+            // The candidate window hides the composing text (a.k.a. preedit).
+            let (cursor_y, cursor_height) =
+                if cfg!(not(any(target_os = "windows", target_os = "macos"))) {
+                    (cursor.y + cursor.height, 0.0)
+                } else {
+                    (cursor.y, cursor.height)
+                };
             self.raw.set_ime_cursor_area(
                 LogicalPosition::new(cursor.x, cursor_y),
                 LogicalSize::new(cursor.width, cursor_height),
