bd6d657737ed3648283031c55c55cab6570e3eb7
Daniel M Bomfim <daniellsmv@hotmailcom>
2026-08-28 20:52:53 -0300

    feat(Viewer): add support for zoom and drag with touch events

--- widget/src/image/viewer.rs
+++ widget/src/image/viewer.rs
@@ -4,7 +4,10 @@ use crate::core::image::{self, FilterMethod};
 use crate::core::layout;
 use crate::core::mouse;
 use crate::core::renderer;
+use crate::core::time;
+use crate::core::touch;
 use crate::core::widget::tree::{self, Tree};
+use crate::core::window;
 use crate::core::{
     ContentFit, Element, Event, Image, Layout, Length, Pixels, Point, Radians, Rectangle, Shell,
     Size, Vector, Widget,
@@ -21,6 +24,7 @@ pub struct Viewer<Handle> {
     handle: Handle,
     filter_method: FilterMethod,
     content_fit: ContentFit,
+    hold_delay: time::Duration,
 }
 
 impl<Handle> Viewer<Handle> {
@@ -36,6 +40,7 @@ impl<Handle> Viewer<Handle> {
             scale_step: 0.10,
             filter_method: FilterMethod::default(),
             content_fit: ContentFit::default(),
+            hold_delay: time::Duration::from_secs(1),
         }
     }
 
@@ -91,6 +96,16 @@ impl<Handle> Viewer<Handle> {
     /// Default is `0.10`
     pub fn scale_step(mut self, scale_step: f32) -> Self {
         self.scale_step = scale_step;
+        self
+    }
+
+    /// Sets the delay between a user pressing a finger and dragging being
+    /// enabled on touch.
+    ///
+    /// Default is `1 sec`
+    pub fn hold_delay(mut self, delay: impl Into<time::Duration>) -> Self {
+        self.hold_delay = delay.into();
+
         self
     }
 }
@@ -160,6 +175,22 @@ where
         let bounds = layout.bounds();
 
         match event {
+            Event::Window(window::Event::RedrawRequested(now)) => {
+                let state = tree.state.downcast_mut::<State>();
+
+                if state.hold_delay.is_none_or(|deadline| *now < deadline) {
+                    return;
+                }
+
+                if !(state.first_finger.is_some() ^ state.second_finger.is_some()) {
+                    state.hold_delay = None;
+                    state.holding_finger = false;
+                    return;
+                }
+
+                state.starting_offset = state.current_offset;
+                state.holding_finger = true;
+            }
             Event::Mouse(mouse::Event::WheelScrolled { delta }) => {
                 let Some(cursor_position) = cursor.position_over(bounds) else {
                     return;
@@ -265,6 +296,124 @@ where
                     shell.capture_event();
                 }
             }
+            Event::Touch(touch::Event::FingerPressed { id, position }) => {
+                if !bounds.contains(*position) {
+                    return;
+                }
+
+                let state = tree.state.downcast_mut::<State>();
+                state.register_finger(*id, *position);
+                state.hold_delay = Some(time::Instant::now() + self.hold_delay);
+                shell.request_redraw_at(window::RedrawRequest::At(state.hold_delay.unwrap()));
+            }
+            Event::Touch(touch::Event::FingerLifted { id, .. })
+            | Event::Touch(touch::Event::FingerLost { id, .. }) => {
+                let state = tree.state.downcast_mut::<State>();
+                state.remove_finger(*id);
+                state.holding_finger = false;
+
+                if state.first_finger.is_none() && state.second_finger.is_none() {
+                    state.pinch_start_distance = None;
+                    state.pinch_anchor = None;
+                }
+            }
+            Event::Touch(touch::Event::FingerMoved { id, position }) => {
+                let state = tree.state.downcast_mut::<State>();
+
+                if state.holding_finger {
+                    let Some(origin) = state
+                        .first_finger_last_position
+                        .or(state.second_finger_last_position)
+                    else {
+                        return;
+                    };
+
+                    let scaled_size = scaled_image_size(
+                        renderer,
+                        &self.handle,
+                        state,
+                        bounds.size(),
+                        self.content_fit,
+                    );
+                    let hidden_width = (scaled_size.width - bounds.width / 2.0).max(0.0).round();
+
+                    let hidden_height =
+                        ((scaled_size.height - bounds.height).max(0.0) / 2.0).round();
+
+                    let delta = *position - origin;
+
+                    let x = if bounds.width < scaled_size.width {
+                        (state.starting_offset.x - delta.x).clamp(-hidden_width, hidden_width)
+                    } else {
+                        0.0
+                    };
+
+                    let y = if bounds.height < scaled_size.height {
+                        (state.starting_offset.y - delta.y).clamp(-hidden_height, hidden_height)
+                    } else {
+                        0.0
+                    };
+
+                    state.current_offset = Vector::new(x, y);
+                    state.update_finger_position(*id, *position);
+
+                    shell.request_redraw();
+                    shell.capture_event();
+                    return;
+                }
+
+                let Some(old_distance) = state.fingers_distance() else {
+                    return;
+                };
+
+                if state.pinch_start_distance.is_none() {
+                    state.pinch_start_distance = Some(old_distance);
+                    state.pinch_start_scale = state.scale;
+                    state.pinch_start_offset = state.current_offset;
+                    state.pinch_anchor = state.fingers_center();
+                }
+
+                state.update_finger_position(*id, *position);
+
+                let Some(new_distance) = state.fingers_distance() else {
+                    return;
+                };
+
+                let start_distance = state.pinch_start_distance.unwrap_or(new_distance);
+                let next_scale = (state.pinch_start_scale * (new_distance / start_distance))
+                    .clamp(self.min_scale, self.max_scale);
+
+                let scale_factor = next_scale / state.pinch_start_scale;
+                state.scale = next_scale;
+
+                let scaled_size = scaled_image_size(
+                    renderer,
+                    &self.handle,
+                    state,
+                    bounds.size(),
+                    self.content_fit,
+                );
+
+                let pinch_anchor = state.pinch_anchor.unwrap_or(bounds.center());
+                let pinch_to_center = pinch_anchor - bounds.center();
+                let adjustment = pinch_to_center * (scale_factor - 1.0)
+                    + state.pinch_start_offset * (scale_factor - 1.0);
+
+                state.current_offset = Vector::new(
+                    if scaled_size.width > bounds.width {
+                        state.pinch_start_offset.x + adjustment.x
+                    } else {
+                        0.0
+                    },
+                    if scaled_size.height > bounds.height {
+                        state.pinch_start_offset.y + adjustment.y
+                    } else {
+                        0.0
+                    },
+                );
+
+                shell.request_redraw();
+            }
             _ => {}
         }
     }
@@ -352,6 +501,16 @@ pub struct State {
     starting_offset: Vector,
     current_offset: Vector,
     cursor_grabbed_at: Option<Point>,
+    first_finger: Option<touch::Finger>,
+    second_finger: Option<touch::Finger>,
+    first_finger_last_position: Option<Point>,
+    second_finger_last_position: Option<Point>,
+    hold_delay: Option<time::Instant>,
+    holding_finger: bool,
+    pinch_start_distance: Option<f32>,
+    pinch_start_scale: f32,
+    pinch_start_offset: Vector,
+    pinch_anchor: Option<Point>,
 }
 
 impl Default for State {
@@ -361,6 +520,16 @@ impl Default for State {
             starting_offset: Vector::default(),
             current_offset: Vector::default(),
             cursor_grabbed_at: None,
+            first_finger: None,
+            second_finger: None,
+            first_finger_last_position: None,
+            second_finger_last_position: None,
+            hold_delay: None,
+            holding_finger: false,
+            pinch_start_distance: None,
+            pinch_start_scale: 1.0,
+            pinch_start_offset: Vector::default(),
+            pinch_anchor: None,
         }
     }
 }
@@ -387,6 +556,65 @@ impl State {
     /// Returns if the cursor is currently grabbed by the [`Viewer`].
     pub fn is_cursor_grabbed(&self) -> bool {
         self.cursor_grabbed_at.is_some()
+    }
+
+    /// Register when a finger and its position
+    fn register_finger(&mut self, finger: touch::Finger, position: Point) {
+        if self.first_finger.is_none() {
+            self.first_finger = Some(finger);
+            self.first_finger_last_position = Some(position);
+            return;
+        }
+
+        if self.second_finger.is_none() {
+            self.second_finger = Some(finger);
+            self.second_finger_last_position = Some(position);
+        }
+    }
+
+    /// Removes the register of a finger touch
+    fn remove_finger(&mut self, finger: touch::Finger) {
+        if self.first_finger.is_some_and(|value| value == finger) {
+            self.first_finger = None;
+            self.first_finger_last_position = None;
+            return;
+        }
+
+        if self.second_finger.is_some_and(|value| value == finger) {
+            self.second_finger = None;
+            self.second_finger_last_position = None;
+        }
+    }
+
+    /// Updates the position of a finger
+    fn update_finger_position(&mut self, finger: touch::Finger, position: Point) {
+        if self.first_finger.is_some_and(|value| value == finger) {
+            self.first_finger_last_position = Some(position);
+            return;
+        }
+
+        if self.second_finger.is_some_and(|value| value == finger) {
+            self.second_finger_last_position = Some(position);
+        }
+    }
+
+    /// When two fingers are regitered returns the distance between them, otherwise returns None
+    fn fingers_distance(&self) -> Option<f32> {
+        Some(
+            self.first_finger_last_position?
+                .distance(self.second_finger_last_position?),
+        )
+    }
+
+    /// When two fingers are regitered returns the center point between them, otherwise returns None
+    fn fingers_center(&self) -> Option<Point> {
+        let first_position = self.first_finger_last_position?;
+        let second_position = self.second_finger_last_position?;
+
+        Some(Point {
+            x: (first_position.x + second_position.x) / 2.0,
+            y: (first_position.y + second_position.y) / 2.0,
+        })
     }
 }
 
